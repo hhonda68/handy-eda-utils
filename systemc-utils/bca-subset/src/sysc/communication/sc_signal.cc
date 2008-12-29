aa0cc20fb4318d113fb121af2f433bc39dfe9b94
hhonda68 <hhonda68@36508731-a755-0410-858a-473cdb9bda21>
2008-12-29 02:45:11 +0000

    avoid including standard header files in bcasysc headers

---
+++ systemc-utils/bca-subset/src/sysc/communication/sc_signal.cc
@@ -0,0 +1,28 @@
+// -*- c++ -*-
+
+//
+// Minimal synthesizable BCA subset of SystemC.
+//
+
+// Copyright (c) 2008 the handy-eda-utils developer(s).
+// Distributed under the MIT License.
+// (See accompanying file COPYING or copy at
+//  http://www.opensource.org/licenses/mit-license.php.)
+
+#include "sc_signal.h"
+#include <vector>
+
+namespace sc_core {
+
+struct sc_in_ports_generic::impl_t {
+  ::std::vector<value_type> vec;
+};
+
+sc_in_ports_generic::sc_in_ports_generic() : m(*(new impl_t)) {}
+sc_in_ports_generic::~sc_in_ports_generic() { delete &m; }
+
+int sc_in_ports_generic::size()                           { return m.vec.size(); }
+void sc_in_ports_generic::push_back(void *x)              { m.vec.push_back(x); }
+void * const sc_in_ports_generic::operator[](int n) const { return m.vec[n]; }
+
+} // namespace sc_core
